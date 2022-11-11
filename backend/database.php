6ead051155180d9ada8fccac5080a96cff16eeb1
Przemek Ćwiertka <przemek.cwiertka@gmail.com>
2022-11-11 17:29:21 +0100

    login

--- backend/database.php
+++ backend/database.php
@@ -9,6 +9,3 @@ $port = "8889";
 $conn = new mysqli($servername, $username, $password, $dbname, $port);
 $q = "SELECT * FROM users";
 
-if ( $result = $conn->query($q) ){
-    $row = $result->fetch_assoc();
-}
--- backend/login.php
+++ backend/login.php
@@ -5,9 +5,18 @@ include 'database.php';
 session_start();
 
 if( !empty($_POST['password']) and !empty($_POST['login'])){
-    if (password_verify($_POST['password'], $row['password']) and $_POST['login'] == $row['login']){
-        $_SESSION['user'] = htmlspecialchars($_POST['login']);
+
+    if ( $result = $conn->query($q) ){
+        
+        while ( $row = $result->fetch_assoc() ){
+
+            if (password_verify($_POST['password'], $row['password']) and $_POST['login'] == $row['login']){
+                $_SESSION['user'] = htmlspecialchars($_POST['login']);
+            }
+
+        }
     }
+
 }
 
 header("Location: /php");
